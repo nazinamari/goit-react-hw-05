dbfe6449e8ee1ef1a920fd5a3627e03a9e1969ff
nazinamari <93985053+nazinamari@users.noreply.github.com>
2024-03-16 17:16:58 +0000

    id

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,6 +3,7 @@ import { Route, Routes } from 'react-router-dom';
 import { Suspense, lazy } from 'react';
 import HomePage from "../../pages/HomePage/HomePage.jsx";
 import MoviesPage from "../MoviesFilter/MoviesFilter.jsx";
+import MovieDetailsPage from "../../pages/MovieDetailsPage/MovieDetailsPage.jsx";
 const NotFoundPage = lazy(() => import('../../pages/NotFoundPage/NotFoundPage.jsx'));
 
 export default function App () {
@@ -13,7 +14,7 @@ export default function App () {
                 <Routes>
                     <Route path="/" element={<HomePage/>}/>
                     <Route path="/movies" element={<MoviesPage/>}/>
-                    <Route path="/movies/:movieId">
+                    <Route path="/movies/:movieId" element={<MovieDetailsPage/>}>
                         {/* <Route path="cast"/>
                         <Route path="reviews"/> */}
                     </Route>
--- src/pages/MovieDetailsPage/MovieDetailsPage.jsx
+++ src/pages/MovieDetailsPage/MovieDetailsPage.jsx
@@ -1,8 +1,12 @@
+import { useParams } from "react-router-dom"
+
 export default function MovieDetailsPage () {
+
+    const { movieId } = useParams()
     
     return (
         <div>
-            <h1>Movie</h1>
+            <h1>Movie {movieId}</h1>
         </div>
     )
 }
